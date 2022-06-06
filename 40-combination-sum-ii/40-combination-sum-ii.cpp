49292f50cb31a8964f2f3cbd3d0ac0b3417f471d
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-06-06 02:17:34 -0400

    Time: 68 ms (9.12%), Space: 16.4 MB (11.97%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    vector<vector<int>> results;
+    vector<bool> visited;
+    
+    void helper(vector<int>& candidates, vector<int> comb, int target, int idx){
+        if(target == 0){
+            results.push_back(comb);
+            return;
+        }else if(target < 0){
+            return;
+        }
+        
+        for(int i = idx; i < candidates.size(); i++){
+            if(!visited[i]){
+                if(i != 0 && candidates[i] == candidates[i-1] && i != idx){
+                    continue;
+                }
+                comb.push_back(candidates[i]);
+                helper(candidates, comb, target - candidates[i], i+1);
+                comb.pop_back();  
+            }
+
+        }
+        
+        
+    }
+    
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> comb;
+        visited = vector<bool>(candidates.size(), false);
+        sort(candidates.begin(), candidates.end());
+        helper(candidates, comb, target, 0);
+        return results;
+    }
+};
