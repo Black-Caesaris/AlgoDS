86ccaa6d99a6ff8f35e367ae00d0cccf810be739
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-07-31 12:48:26 -0400

    Time: 0 ms (100.00%), Space: 6.7 MB (37.76%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -1,52 +1,75 @@
 class Solution {
 public:
+    
+    string duplicate(int x, string s){
+        string duplicate = "";
+        for(int i = 0 ; i < x; i++){
+            duplicate += s;
+        }
+        return duplicate;
+    }
+    
+    string formNumber(int index, string s){
+        string n;
+        int number = 0;
+        int base = 1;
+        
+        while(index < s.length() && isdigit(s[index])){
+            n.push_back(s[index]);
+            index++;
+        }
+            
+        reverse(begin(n), end(n));
+
+        for(char &d : n){
+            number += base * (d-'0');
+            base = base*10;
+        }
+        
+        // cout << number << endl;
+        return to_string(number);
+    }
+    
     string decodeString(string s) {
-        int n = s.length();
-        
-        if(n == 0) return "";
-        
         stack<string> stk;
-        
-        for(int i = 0 ; i < n; i++){
-            if(s[i] != ']'){
-                stk.push(string(1, s[i]));
-            }else{
+ 
+        for(int i = 0 ; i < s.length(); i++){
+            if(s[i] == ']'){
                 string temp;
+                
+                // pop from stack, and then push back number of times
                 while(!stk.empty() && stk.top() != "["){
                     temp += stk.top();
                     stk.pop();
                 }
+                         
+                // pop out '['
+                stk.pop();                
+                
+                // duplicate x times
+                int x = stoi(stk.top());
                 stk.pop();
-                
-                int base = 1;
-                int n = 0;
-                
-                while(!stk.empty() && isdigit(stk.top()[0])){
-                    n = n + stoi(stk.top())*base;
-                    base = base * 10;
-                    stk.pop();
-                }
-                
-                
-                string f;
-                for(int j = 0 ; j < n; j++){
-                    for(int i = temp.size() - 1; i >= 0 ; i--){
-                        stk.push(string(1, temp[i]));
-                    }
-                }
-                stk.push(f);
+                string dup = duplicate(x, temp);
+                cout << dup;
+                stk.push(dup);
+            } else if(isdigit(s[i])){
+                string number = formNumber(i, s);
+                i = i + number.length() - 1;
+                stk.push(number);
+            }else{
+               stk.push(string(1,s[i]));   
             }
+            
+            cout << endl;
         }
         
-        
-        string r;
-    
-        for(int i = stk.size() - 1; i >=0 ; i--){
-            r = stk.top() + r;
+        string decodedString;
+        while(!stk.empty()){
+            decodedString += stk.top();
             stk.pop();
         }
         
-        
-        return r;
+        reverse(begin(decodedString), end(decodedString));
+        return decodedString;
     }
 };
