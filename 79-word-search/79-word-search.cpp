13f3dbc7ca5585321d78f8e15d5aedde17a45702
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-08-07 16:33:08 -0700

    Time: 638 ms (49.25%), Space: 8.1 MB (37.71%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -9,29 +9,28 @@ public:
         if(n >= word.length()){
             return true;
         }
+            
         
-        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[n]){
-            return false;
-        }
-        
+        char c = board[i][j];
         board[i][j] = '#';
-        bool result = false;
         
         for(int k = 0 ; k < 4; k++){
-            
             int x = i + x_coord[k];
             int y = j + y_coord[k];
             
-            result = dfs(board, x, y, word, n+1);
-                
-            if(result){
-                return true;
+            if(x >= 0 && y >= 0 && x < board.size() && y < board[0].size()){
+                if(board[x][y] == word[n]){
+                    if(dfs(board, x, y, word, n+1)){
+                        return true;
+                    }
+                }
+
             }
         }
     
-        board[i][j] = word[n];
+        board[i][j] = c;
         
-        return result;
+        return false;
     }
     
     bool exist(vector<vector<char>>& board, string word) {
@@ -40,11 +39,13 @@ public:
 
         for(int i = 0 ; i < board.size(); i++){
             for(int j = 0 ; j < board[0].size(); j++){
-                    if(dfs(board,i, j, word, 0)){
-                            return true;
+                if(board[i][j] == word[0]){
+                    if(dfs(board,i, j, word, 1)){
+                        return true;
                     }
                 }
             }
+        }
         
         return false;
     }
