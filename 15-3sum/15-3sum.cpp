a3f132394b211a422e9f37f7be0fbaa7284720f1
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-09-23 06:38:38 -0700

    Time: 246 ms (27.68%), Space: 23.5 MB (26.36%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
-        if(nums.size() < 3) return result;
-        
-        // sort
         sort(nums.begin(), nums.end());
         int left, right;
-        for(int i = 0 ; i < nums.size() - 2 && nums[i] <= 0; i++){
-            if(i > 0 && nums[i] == nums[i-1]){
+        vector<vector<int>> result;
+        
+        for(int i = 0 ; i < nums.size() - 2; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
-            left = i + 1;
-            right = nums.size() - 1;
             
-            while(left < right){
-                if(nums[i] + nums[left] + nums[right] == 0){
-                    result.push_back({nums[i], nums[left++], nums[right--]});
-                    while(left < right && nums[left] == nums[left-1]){
+                left = i + 1;
+                right = nums.size() - 1;
+                while(left < right) {
+                    if(nums[i] + nums[left] + nums[right] < 0 ) {
                         left++;
+                    } else if (nums[i] + nums[left] + nums[right] > 0) {
+                        right--;
+                    } else {
+                        vector<int> temp;
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[left++]);
+                        temp.push_back(nums[right--]);
+                        result.push_back(temp);
+                        while(left < right && nums[left] == nums[left-1]){
+                            left++;
+                        }
                     }
-                }else if(nums[i] + nums[left] + nums[right] < 0){
-                    left++;
-                }else{
-                    right--;
                 }
-            } 
         }
-        
         return result;
     }
 };
