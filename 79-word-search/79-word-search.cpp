17cdbc1befc381d93047410c4f7ba599e2bb1ae1
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-09-26 15:25:30 -0400

    Time: 997 ms (26.69%), Space: 8.2 MB (18.69%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,48 +1,52 @@
 class Solution {
 public:
     
-    vector<int> x_coord = {0, 0,-1, 1};
+    vector<int> x_coord = {0, 0, 1, -1};
     vector<int> y_coord = {-1, 1, 0, 0};
     
-    bool dfs(vector<vector<char>>& board, int i, int j, string& word, int n) {
-
-        if(n >= word.length()){
+    bool inBounds(int x, int y, vector<vector<char>>& board) {
+        if(x >= 0 && x < board.size() && y >=0 && y < board[0].size()) {
             return true;
         }
-            
-        
-        char c = board[i][j];
-        board[i][j] = '#';
-        
-        for(int k = 0 ; k < 4; k++){
-            int x = i + x_coord[k];
-            int y = j + y_coord[k];
-            
-            if(x >= 0 && y >= 0 && x < board.size() && y < board[0].size()){
-                if(board[x][y] == word[n]){
-                    if(dfs(board, x, y, word, n+1)){
-                        return true;
-                    }
-                }
-
-            }
-        }
-    
-        board[i][j] = c;
         
         return false;
     }
     
-    bool exist(vector<vector<char>>& board, string word) {
+    bool dfs(vector<vector<char>>& board, int x, int y, int idx, string& word, vector<vector<bool>>& visited) {
+        if(idx >= word.length()) {
+            return true;
+        }
         
-        vector<vector<int>> visited(board.size(), vector<int>(board[0].size(), 0));
+        if(!inBounds(x, y, board)) return false;
+        if(visited[x][y] || board[x][y] != word[idx]) return false;
+        
 
-        for(int i = 0 ; i < board.size(); i++){
-            for(int j = 0 ; j < board[0].size(); j++){
-                if(board[i][j] == word[0]){
-                    if(dfs(board,i, j, word, 1)){
-                        return true;
-                    }
+        
+        visited[x][y] = true;
+        
+        for(int i = 0 ; i < 4; i++) {
+            int x_new = x + x_coord[i];
+            int y_new = y + y_coord[i];
+            if(dfs(board, x_new, y_new, idx+1, word, visited)){
+                return true;
+            }
+        }
+        
+        return visited[x][y] = false;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        int r = board.size();
+        int c = board[0].size();
+        
+        if(!r && !c) return false;
+        
+        vector<vector<bool>> visited(r, vector<bool>(c, false));
+        
+        for(int i = 0; i < board.size(); i++) {
+            for(int j = 0 ; j < board[0].size(); j++) {
+                if(dfs(board, i, j, 0, word, visited)){
+                    return true;
                 }
             }
         }
