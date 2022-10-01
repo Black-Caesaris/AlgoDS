0d642cf537cfe52bef582640f75be4cf0957c286
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-10-01 18:41:59 -0400

    Time: 12 ms (18.97%), Space: 8.8 MB (50.38%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -5,12 +5,12 @@ public:
         int right = nums.size() - 1;
         while(left < right) {
             int mid = left + (right-left)/2;
-             if (nums[mid] > nums[mid+1]) {
+            if (nums[mid] > nums[mid+1]) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
-        return left;
+        return right;
     }
 };
