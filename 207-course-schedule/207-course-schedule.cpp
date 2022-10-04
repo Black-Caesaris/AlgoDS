5a694b66aae4e83664df856b7a00adc7f350908d
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-10-04 09:26:40 -0400

    Time: 39 ms (54.80%), Space: 14.7 MB (24.49%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     unordered_map<int, vector<int>> graph;
-    unordered_set<int> recursion_stack;
-    unordered_set<int> visited;
+    // unordered_set<int> recursion_stack;
+    // unordered_set<int> visited;
     
-    bool dfs(int node) {
+    bool dfs(int node, vector<int>& recursion_stack, vector<int>& visited) {
         
-        if(visited.count(node))
+        // if(visited.count(node))
+        //     return true;
+        
+        // if(recursion_stack.count(node))
+        //     return false;
+        if(visited[node])
             return true;
         
-        if(recursion_stack.count(node))
+        if(recursion_stack[node])
             return false;
 
-        recursion_stack.insert(node);
+        // recursion_stack.insert(node);
+        recursion_stack[node] = true;
         
         bool ret = true;
         for(auto i = graph[node].begin(); i!= graph[node].end(); i++) {
-            ret &= dfs(*i);
+            ret &= dfs(*i, recursion_stack, visited);
             if(!ret)
                 return ret;
         }
         
-        visited.insert(node);
-        recursion_stack.erase(node);
+//         visited.insert(node);
+//         recursion_stack.erase(node);
+        visited[node] = true;
+        recursion_stack[node] = false;
+        
         return ret;
         
     }
@@ -43,9 +52,16 @@ public:
         
         int nodes = graph.size();
 
+//         unordered_set<int> recursion_stack;
+//         unordered_set<int> visited;
+//         int *recursion_stack = new int[numCourses];
+//         int *visited = new int[numCourses];
+        
+        vector<int> recursion_stack(numCourses, false);
+        vector<int> visited(numCourses, false);         
         
         for(auto i = graph.begin(); i != graph.end(); i++) {
-            if(!dfs(i->first))
+            if(!dfs(i->first, recursion_stack, visited))
                 return false;
         }
         
