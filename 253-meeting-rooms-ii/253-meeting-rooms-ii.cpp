140325d6a273e2e7faf7138e5b38dc9dfb71ee0e
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-10-05 16:44:41 -0400

    Time: 40 ms (39.21%), Space: 12.2 MB (49.82%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        if(intervals.size() == 0) return 0;
-            
-        sort(begin(intervals), end(intervals), [](vector<int>& a, vector<int>& b){
-            return (a[0] < b[0]);
+        priority_queue<int, vector<int>, greater<int>> heap;
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b){
+            return a[0] < b[0];
         });
-        
-        
             
-        priority_queue<int, vector<int>, greater<int>> q;
-        
-        q.push(intervals[0][1]);
-        for(auto i = 1 ; i < intervals.size(); i++){
-            if(q.top() <= intervals[i][0]){
-                q.pop();
+        for(int i = 0 ; i < intervals.size(); i++) {
+            vector<int> interval = intervals[i];
+            
+            if(!heap.empty() && heap.top() <= interval[0]) {
+                heap.pop();
             }
-            
-            q.push(intervals[i][1]);
-
+            heap.push(interval[1]);
         }
         
-        return q.size();
+        return heap.size();
     }
 };
