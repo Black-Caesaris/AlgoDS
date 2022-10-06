1c38d2ef01b6e17b349bb3f985b1a83e4d77654f
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-10-06 09:37:42 -0400

    Time: 2 ms (58.20%), Space: 7 MB (59.20%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,48 +1,69 @@
 class Solution {
 public:
+    vector<int> x_coord = {0, 0, 1, -1, 1, 1, -1, -1, };
+    vector<int> y_coord = {1, -1, 0, 0, 1, -1, 1, -1};
     
-    vector<vector<int>> copy;
-
-    vector<int> x_dir = {0,0,1,-1,1,1,-1,-1};
-    vector<int> y_dir = {-1, 1, 0, 0,1,-1,1,-1};
-    
-    void mutateCell(vector<vector<int>>& board, int x, int y){
-        int live = 0;
-        int dead = 0;
-        
-        for(int i = 0 ; i < 8; i++){
-            int x_new = x + x_dir[i];
-            int y_new = y + y_dir[i];
-            
-            if(x_new >= 0 && y_new >= 0 && x_new < board.size() && y_new < board[0].size()){
-                if(copy[x_new][y_new] == 1){
-                    live++;
-                } else {
-                    dead++;
-                }
-            }    
+    bool isValid (int x, int y, vector<vector<int>>& board) {
+        if(x >= 0 && x < board.size() && y >= 0 && y < board[0].size()) {
+            return true;
         }
-        
-        if(board[x][y]){
-            if(live < 2 || (live > 3)){
-                board[x][y] = 0;
-            }
-        } else {
-            if(live == 3) {
-                board[x][y] = 1;
-            }
-        }
-    
+        return false;
     }
     
-    void gameOfLife(vector<vector<int>>& board) {
-        copy = board;
+    
+    void mutateCell(vector<vector<int>>& board, int x, int y) {
         
-        for(int i = 0 ; i < board.size(); i++){
-            for(int j = 0 ; j < board[0].size(); j++){
+        int live_n = 0;
+        int dead_n = 0;
+        
+        for(int i = 0 ; i <= 7 ; i++) {
+            int x_new = x + x_coord[i];
+            int y_new = y + y_coord[i];
+            
+            if(isValid(x_new, y_new, board)) {
+                if(board[x_new][y_new] == 0 || board[x_new][y_new] == -1) {
+                    dead_n++;
+                } else {
+                    live_n++;
+                }
+            }
+        }
+        
+        if(board[x][y] && (live_n < 2 || live_n > 3)) {
+            board[x][y] = -2;
+        } else if (!board[x][y] && live_n == 3) {
+            board[x][y] = -1;
+        }
+        
+    }
+        
+    void gameOfLife(vector<vector<int>>& board) {
+        // states = {0, 1, }
+        
+        // 1. live < 2(LN) = Dead
+        // 2. live == 2(LN) || 3(LN) = live
+        // 3. live > 3(LN) = dead
+        // 4. dead == 3(LN) == alove
+        
+        int r = board.size();
+        int c = board[0].size();
+        
+        for(int i = 0 ; i < r; i++) {
+            for(int j = 0 ; j < c; j++) {
                 mutateCell(board, i, j);
             }
-        }        
+        }
         
-    }
+        for(int i = 0 ; i < r; i++) {
+            for(int j = 0 ; j < c; j++) {
+                if(board[i][j] == -1)
+                    board[i][j] = 1;
+                else if(board[i][j] == -2)
+                    board[i][j] = 0;
+            }
+        }
+        
+        
+        
+      }
 };
