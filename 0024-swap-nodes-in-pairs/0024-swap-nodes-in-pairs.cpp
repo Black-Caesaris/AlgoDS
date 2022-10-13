ce7987bf563219d2f0355f24a7b203ac00df65da
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-10-13 09:31:01 -0400

    Time: 0 ms (100.00%), Space: 7.6 MB (54.75%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -10,43 +10,43 @@
  */
 class Solution {
 public:
-// Iterative solution
-//     ListNode* swapPairs(ListNode* head) {
-//         if(!head || !head->next)
-//             return head;
-        
-//         ListNode* dummyHead = new ListNode(-1);
-//         dummyHead->next = head;
-        
-//         ListNode* ptr1 = dummyHead;
-//         ListNode* prev = head;
-//         ListNode* curr = head->next;
-//         ListNode* next;
-        
-//         while(curr) {
-//             next = curr->next;
-//             curr->next = prev;
-//             prev->next = next;
-//             ptr1->next = curr;
-//             ptr1 = prev;
-//             prev = prev->next;
-//             if(!prev)
-//                 break;
-//             curr = prev->next;
-//         }
-        
-//         return dummyHead->next;
-//     }
-    
-    // A much simpler solution using recursion (top voted answer)
+//Iterative solution (faster than recursion)
     ListNode* swapPairs(ListNode* head) {
         if(!head || !head->next)
             return head;
         
-        ListNode* temp = head->next;
+        ListNode* dummyHead = new ListNode(-1);
+        dummyHead->next = head;
         
-        head->next = swapPairs(head->next->next);
-        temp->next = head;
-        return temp;
+        ListNode* ptr1 = dummyHead;
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+        ListNode* next;
+        
+        while(curr) {
+            next = curr->next;
+            curr->next = prev;
+            prev->next = next;
+            ptr1->next = curr;
+            ptr1 = prev;
+            prev = prev->next;
+            if(!prev)
+                break;
+            curr = prev->next;
+        }
+        
+        return dummyHead->next;
     }
+    
+    // A much simpler solution using recursion (top voted answer)
+//     ListNode* swapPairs(ListNode* head) {
+//         if(!head || !head->next)
+//             return head;
+        
+//         ListNode* temp = head->next;
+        
+//         head->next = swapPairs(head->next->next);
+//         temp->next = head;
+//         return temp;
+//     }
 };
