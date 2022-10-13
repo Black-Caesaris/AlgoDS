b7355d511c70c8b9f716f17a6941daabfba4c121
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-10-13 19:06:49 -0400

    Time: 3 ms (52.51%), Space: 8 MB (5.70%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,8 +1,6 @@
-typedef vector<vector<string>> vvs;
 typedef list<list<string>> lls;
-typedef vector<string> vs;
 typedef list<string> ls;
-
+typedef vector<string> vs;
 
 class Solution {
 public:
@@ -12,7 +10,6 @@ public:
     
     lls backtrack(string& s) {
         if(s.length() == 0) {
-            cout << "In";
             lls result;
             result.push_back({});
             return result;
@@ -22,16 +19,11 @@ public:
             return dp[s];
         }
         
-        // cout << s << endl;
         for(int i = 1 ; i <= s.length(); i++) {
             string prefix = s.substr(0, i);
-            // cout << prefix << " " << endl;
             if(wordSet.count(prefix)) {
-                cout << prefix << endl;
                 string next = s.substr(i);
-                lls sentences = backtrack(next);
-                // cout << sentences.size();
-                
+                lls sentences = backtrack(next);                
                 // in each sentence add the current prefix
                 for(ls sentence : sentences) {
                     sentence.push_front(prefix);
@@ -44,7 +36,7 @@ public:
         
     }    
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        ls result;
+        vs result;
         wordSet = unordered_set<string>(wordDict.begin(), wordDict.end());
         backtrack(s);
         
@@ -57,12 +49,10 @@ public:
                 temp += word;
                 temp += " ";
             }
-            cout << temp;
             temp.pop_back();
             result.push_back(temp);
         }
         
-        vector<string> resultVector(result.begin(), result.end());
-        return resultVector;
+        return result;
     }
 };
