017bb54113d90919911de92e8b785b448747f54f
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-10-19 09:48:39 -0400

    Time: 537 ms (25.39%), Space: 63.4 MB (24.44%) - LeetHub

---
+++ 0818-race-car/0818-race-car.cpp
@@ -0,0 +1,50 @@
+typedef pair<int, int> pii;
+
+class Solution {
+    
+public:
+    
+    set<pii> v;
+    
+    void sol(queue<pii>& q, int position, int speed, int target) {
+        pii node = {position, speed};
+        if(position >= 0 && position < 2 * target && !v.count(node)) {
+            q.push(node);
+            v.insert(node);
+        }    
+    }
+    
+    int racecar(int target) {
+        queue<pii> q;
+
+        // initial (position, speed)
+        q.push({0, 1});
+        int size = 1;
+        int result = 0;
+
+
+        while(!q.empty()) {
+            pii node = q.front();
+            q.pop();
+            
+            int position = node.first;
+            int speed = node.second;
+            
+            if(position == target)
+                return result;
+            
+            
+            sol(q, position + speed, speed * 2, target);
+            
+            sol(q, position, speed > 0 ? - 1 : 1 , target);
+            
+            if(--size == 0) {
+                size = q.size();
+                result++;
+            }
+        
+        }
+        
+        return 0;
+    }
+};
