12416b389143dd34850769d7e51b10463dc12de0
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-10-28 09:11:10 -0400

    Time: 3 ms (93.71%), Space: 7 MB (22.00%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
+        
+        if(s.length() != goal.length())
+            return false;
+        
         int charSet[26] = {0};
+        
         if(s == goal) {
             for(int i = 0 ; i < s.length(); i++) {
                 if(charSet[s[i] - 'a'] == 1)
@@ -12,24 +17,15 @@ public:
         }
         
         
-        int first = -1;
-        int second = -1;
-        
+        vector<int> diff;
+            
         for(int i = 0 ; i < goal.length(); i++) {
             if(s[i] != goal[i]) {
-                if(first == -1)
-                    first = i;
-                else
-                    second = i;
+                diff.push_back(i);
             }
         }
-        
-        if(first == -1 || second == -1)
-            return false;
-        
-        swap(s[first], s[second]);
-        
-        return s == goal;
+                
+        return diff.size() == 2 && s[diff[0]] == goal[diff[1]] && s[diff[1]] == goal[diff[0]];
         
     }
 };
