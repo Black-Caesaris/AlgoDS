6016a55b20e11ee4903ee20ec326a5f5ea8ad221
Siddhartha Singh <singh.siddhartha23@gmail.com>
2022-10-29 09:26:34 -0400

    Time: 480 ms (5.00%), Space: 59.4 MB (31.21%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -5,12 +5,9 @@ public:
         int min_so_far = INT_MAX;
         for(int i = 0 ; i < points.size(); i++) {
             if(points[i][0] == x || points[i][1] == y) {
-                cout << "x: " << points[i][0] << " y : " << points[i][1] << endl;
                 int dist = abs(x - points[i][0]) + abs(y - points[i][1]);
-                cout << dist << endl;
                 if(min_so_far > dist) {
                     min_so_far = dist;
-                    cout << "i : " << i << endl;
                     result = i;
                 }
             }
